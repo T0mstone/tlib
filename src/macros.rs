23d20f0407656a7bdabf1bf95fa0addc4a4d5f7d
T0mstone <realt0mstone@gmail.com>
2019-04-16 17:52:27 +0200

    added the crate_version! macro (stolen from clap but I think this should be accessible without depending on clap)

--- src/macros.rs
+++ src/macros.rs
@@ -28,3 +28,17 @@ macro_rules! hashmap(
         }
      };
 );
+
+/// Allows you pull the version for an from your Cargo.toml as MAJOR.MINOR.PATCH_PKGVERSION_PRE
+#[macro_export]
+macro_rules! crate_version {
+    () => {
+        format!(
+            "{}.{}.{}{}",
+            env!("CARGO_PKG_VERSION_MAJOR"),
+            env!("CARGO_PKG_VERSION_MINOR"),
+            env!("CARGO_PKG_VERSION_PATCH"),
+            option_env!("CARGO_PKG_VERSION_PRE").unwrap_or("")
+        )
+    };
+}
