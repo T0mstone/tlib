d15e5dd0e51bee42c5dafae318ff1f94a7dfa583
T0mstone <realt0mstone@gmail.com>
2019-04-16 16:26:56 +0200

    implementedd `Deref<Target=T>` for `Singleton<T>`

--- src/singleton.rs
+++ src/singleton.rs
@@ -1,22 +1,33 @@
+use std::ops::Deref;
+
 /// A type that can only be initialized once
 ///
 /// Intended for use with `static mut`
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct Singleton<T> {
-    inner: Option<T>
+    inner: Option<T>,
+}
+
+impl<T> Deref for Singleton<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        self.inner()
+    }
 }
 
 impl<T> Singleton<T> {
     /// Creates a new, uninitialized singleton
     pub const fn new() -> Self {
-        Self {
-            inner: None
-        }
+        Self { inner: None }
     }
 
     fn assert_init(&self, funcname: &str) {
         if !self.is_initialized() {
-            panic!("called `Singleton::{}()` but Singleton is not initialized", funcname);
+            panic!(
+                "called `Singleton::{}()` but Singleton is not initialized",
+                funcname
+            );
         }
     }
 
@@ -31,7 +42,7 @@ impl<T> Singleton<T> {
                 self.inner = Some(t);
                 true
             }
-            _ => false
+            _ => false,
         }
     }
 
@@ -42,7 +53,8 @@ impl<T> Singleton<T> {
 
     /// Converts `Singleton<T>` to `Option<T>`, using `Clone`
     pub fn to_option(&self) -> Option<T>
-        where T: Clone
+    where
+        T: Clone,
     {
         self.inner.clone()
     }
@@ -64,10 +76,10 @@ impl<T> Singleton<T> {
     }
 
     /// Moves the value v out of the `Singleton<T>` if it is initialized.
-     ///
-     /// # Panics
-     ///
-     /// Panics if the value is not initialized with a custom panic message provided by `msg`.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the value is not initialized with a custom panic message provided by `msg`.
     pub fn expect(self, msg: &str) -> T {
         if !self.is_initialized() {
             panic!("{}", msg);
@@ -85,7 +97,7 @@ impl<T> Singleton<T> {
         self.assert_init("inner");
         match self.inner {
             Some(ref v) => v,
-            _ => unreachable!()
+            _ => unreachable!(),
         }
     }
 
@@ -97,30 +109,36 @@ impl<T> Singleton<T> {
         self.assert_init("inner_mut");
         match self.inner {
             Some(ref mut v) => v,
-            _ => unreachable!()
+            _ => unreachable!(),
         }
     }
 
     /// Converts from Singleton<T> to Singleton<&T>
     pub fn as_ref(&self) -> Singleton<&T> {
-        Singleton { inner: self.inner.as_ref() }
+        Singleton {
+            inner: self.inner.as_ref(),
+        }
     }
 
     /// Converts from Singleton<T> to Singleton<&mut T>
     pub fn as_mut(&mut self) -> Singleton<&mut T> {
-        Singleton { inner: self.inner.as_mut() }
+        Singleton {
+            inner: self.inner.as_mut(),
+        }
     }
 
     /// works analogous to [`Option::map`](https://doc.rust-lang.org/std/option/enum.Option.html#method.map)
     pub fn map<F: FnMut(T) -> U, U>(self, f: F) -> Singleton<U> {
-        Singleton { inner: self.inner.map(f) }
+        Singleton {
+            inner: self.inner.map(f),
+        }
     }
 
     /// works like [`map`](#method.map) but modifies `self` instead of returning a new `Singleton`.
     pub fn map_inplace<F: FnMut(T) -> T>(&mut self, mut f: F) {
         let inner = match self.inner.take() {
             Some(t) => Some(f(t)),
-            None => None
+            None => None,
         };
         self.inner = inner;
     }
@@ -130,7 +148,7 @@ impl<T: Clone> Singleton<&T> {
     /// Maps a `Singleton<&T>` to a `Singleton<T>` by cloning the contents of the singleton.
     pub fn cloned(&self) -> Singleton<T> {
         Singleton {
-            inner: self.inner.cloned()
+            inner: self.inner.cloned(),
         }
     }
 }
