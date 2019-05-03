b39f3bae383dd4e04a0f6e2e94b54567a5e22ab6
T0mstone <realt0mstone@gmail.com>
2019-05-03 18:15:53 +0200

    added generated unzip functions, put their generation together with the generation of the zip functions into a huge macro, refactored the unzip function to resemble the generated unzip functions, added a test for unzip10 (representing all unzip functions)

--- src/vec_zip.rs
+++ src/vec_zip.rs
@@ -8,9 +8,9 @@ pub fn zip<T, U>(a: Vec<T>, b: Vec<U>) -> Vec<(T, U)> {
 pub fn unzip<T, U>(v: Vec<(T, U)>) -> (Vec<T>, Vec<U>) {
     let mut a = Vec::new();
     let mut b = Vec::new();
-    for (x, y) in v {
-        a.push(x);
-        b.push(y);
+    for (t1, t2) in v {
+        a.push(t1);
+        b.push(t2);
     }
     (a, b)
 }
@@ -37,7 +37,7 @@ impl<T, U> VecUnzipTrait<T, U> for Vec<(T, U)> {
     }
 }
 
-// Some helper macros to make writing the zip functions way less painful
+// Macros for generating the zip functions
 macro_rules! __flatten1 {
     ($first:ident; $($bind:ident),*) => {
         |($first, ($($bind),*))| ($first, $($bind),*)
@@ -88,22 +88,70 @@ macro_rules! gen_zip_fns_accum {
     };
 }
 
-// TODO: unzip functions up to 10 + add traits for these
+// Macros for generating the unzip functions
 
-gen_zip_fns_accum! {
-    zip3 => t1 @ a: A, t2 @ b: B, t3 @ c: C;
-    zip4 => t4 @ d: D;
-    zip5 => t5 @ e: E;
-    zip6 => t6 @ f: F;
-    zip7 => t7 @ g: G;
-    zip8 => t8 @ h: H;
-    zip9 => t9 @ i: I;
-    zip10 => t10 @ j: J;
+macro_rules! gen_unzip_fn {
+    ($fname:ident => $($binding:ident @ $vname:ident: $t:ident),*) => {
+        pub fn $fname<$($t),*>(v: Vec<($($t),*)>) -> ($(Vec<$t>),*) {
+            $(
+                let mut $vname = Vec::new();
+            )*
+            for ($($binding),*) in v {
+                $(
+                    $vname.push($binding);
+                )*
+            }
+            ($($vname),*)
+        }
+    };
+}
+
+macro_rules! gen_unzip_fns_accum {
+    ($fname:ident => $($binding:ident @ $vname:ident: $t:ident),*; $($fname2:ident => $($binding2:ident @ $vname2:ident: $t2:ident),*;)*) => {
+        gen_unzip_fns_accum!([] $fname => $($binding @ $vname: $t),*; $($fname2 => $($binding2 @ $vname2: $t2),*;)*);
+    };
+    ([$($binding1:ident @ $vname1:ident: $t1:ident,)*] $fname:ident => $($binding:ident @ $vname:ident: $t:ident),*;) => {
+        gen_unzip_fn!($fname => $($binding1 @ $vname1: $t1,)* $($binding @ $vname: $t),*);
+    };
+    ([$($binding1:ident @ $vname1:ident: $t1:ident,)*] $fname:ident => $($binding:ident @ $vname:ident: $t:ident),*; $($fname2:ident => $($binding2:ident @ $vname2:ident: $t2:ident),*;)*) => {
+        gen_unzip_fn!($fname => $($binding1 @ $vname1: $t1,)* $($binding @ $vname: $t),*);
+        gen_unzip_fns_accum!([$($binding1 @ $vname1: $t1,)* $($binding @ $vname: $t,)*] $($fname2 => $($binding2 @ $vname2: $t2),*;)*);
+    };
+}
+
+// Unified macro to generate zip AND unzip functions
+
+macro_rules! gen_zip_and_unzip_fns_accum {
+    ($fname:ident, $ufname:ident => $($binding:ident @ $extra:ident: $t:ident),*; $($fname2:ident, $ufname2:ident => $($binding2:ident @ $extra2:ident: $t2:ident),*;)*) => {
+        gen_zip_and_unzip_fns_accum!([] $fname, $ufname => $($binding @ $extra: $t),*; $($fname2, $ufname2 => $($binding2 @ $extra2: $t2),*;)*);
+    };
+    ([$($binding1:ident @ $extra1:ident: $t1:ident,)*] $fname:ident, $ufname:ident => $($binding:ident @ $extra:ident: $t:ident),*;) => {
+        gen_zip_fn!($fname => $($binding1 @ $extra1: $t1,)* $($binding @ $extra: $t),*);
+        gen_unzip_fn!($ufname => $($binding1 @ $extra1: $t1,)* $($binding @ $extra: $t),*);
+    };
+    ([$($binding1:ident @ $extra1:ident: $t1:ident,)*] $fname:ident, $ufname:ident => $($binding:ident @ $extra:ident: $t:ident),*; $($fname2:ident, $ufname2:ident => $($binding2:ident @ $extra2:ident: $t2:ident),*;)*) => {
+        gen_zip_fn!($fname => $($binding1 @ $extra1: $t1,)* $($binding @ $extra: $t),*);
+        gen_unzip_fn!($ufname => $($binding1 @ $extra1: $t1,)* $($binding @ $extra: $t),*);
+        gen_zip_and_unzip_fns_accum!([$($binding1 @ $extra1: $t1,)* $($binding @ $extra: $t,)*] $($fname2, $ufname2 => $($binding2 @ $extra2: $t2),*;)*);
+    };
+}
+
+// Generated functions
+
+gen_zip_and_unzip_fns_accum! {
+    zip3, unzip3 => t1 @ a: A, t2 @ b: B, t3 @ c: C;
+    zip4, unzip4 => t4 @ d: D;
+    zip5, unzip5 => t5 @ e: E;
+    zip6, unzip6 => t6 @ f: F;
+    zip7, unzip7 => t7 @ g: G;
+    zip8, unzip8 => t8 @ h: H;
+    zip9, unzip9 => t9 @ i: I;
+    zip10, unzip10 => t10 @ j: J;
 }
 
 #[cfg(test)]
 mod test {
-    use super::zip10;
+    use super::{unzip10, zip10};
 
     macro_rules! vecs {
         ($($a:literal, $b:literal),*) => {
@@ -129,5 +177,18 @@ mod test {
             ],
             v
         );
+    }
+
+    #[test]
+    fn test_unzip10() {
+        let (a, b, c, d, e, f, g, h, i, j) =
+            vecs![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+
+        let v = zip10(a, b, c, d, e, f, g, h, i, j);
+
+        assert_eq!(
+            vecs![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+            unzip10(v)
+        )
     }
 }
